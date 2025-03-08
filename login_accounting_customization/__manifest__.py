3d4e46488d6d8fb88543f8098e367285b43c64a5
“eman.saeed” <khalifaeman4@gmail.com>
2025-03-08 15:49:15 +0400

    Login theme accounting

---
+++ login_accounting_customization/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Accounting Login Theme',
+    'version': '1.0',
+    'author': 'Your Name',
+    'category': 'Website',
+    'summary': 'Customizes the login page design and theme.',
+    'depends': ['web','website'],
+    'data': [
+        'views/login_template.xml',
+        'views/assets.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'login_customization/static/src/css/login_styles.css',
+            '/login_customization/static/src/img/account_bg.png',
+        ],
+    },
+    'installable': True,
+    'application': False,
+}
+
