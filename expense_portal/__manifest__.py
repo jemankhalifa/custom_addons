5a0c926bca5557d55217cae36c6b70f3fcf820fb
ibrahimmak <himamak2518@gmail.com>
2025-03-11 08:51:16 +0200

    Modify the changes in last comments

--- expense_portal/__manifest__.py
+++ expense_portal/__manifest__.py
@@ -1,10 +1,11 @@
 {
     'name': 'Custom Expense Portal',
+    'author': 'Net4X Innovation',
     'sequence': -100,
     'version': '1.0',
     'category': 'Human Resources',
     'summary': 'Custom Expense Portal for Employees',
-    'depends': ['hr_expense'],
+    'depends': ['hr_expense', 'account'],
     'data': [
         'security/ir.model.access.csv',
         'security/expenses_group.xml',
