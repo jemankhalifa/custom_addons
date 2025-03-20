a2c1d5679befcdac70d43fd48689090f0cdac7f0
“eman.saeed” <khalifaeman4@gmail.com>
2025-03-20 13:15:15 +0400

    edu theme

---
+++ custom_edu_theme/static/js/custom_scripts.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // تحديد العنصر الأب (<h1>) الذي يحتوي على النص
+    const learnFunText = document.querySelector("h1");
+    
+   // تطبيق تأثير GSAP على العنصر الأب
+    gsap.from(learnFunText, {
+        opacity: 0,
+        y: 50,
+        duration: 2,
+        delay: 0.5,
+        ease: "power2.out"
+    });
+    if (document.body.classList.contains("o_web_client")){
+    const bubbleContainer = document.createElement("div");
+    bubbleContainer.classList.add("bubble-container");
+    document.body.appendChild(bubbleContainer);
+
+    function createBubble() {
+        let bubble = document.createElement("div");
+        bubble.classList.add("bubble");
+        bubble.style.left = Math.random() * 100 + "vw"; // مكان عشوائي أفقيًا
+        bubble.style.animationDuration = Math.random() * 3 + 2 + "s"; // مدة حركة عشوائية
+        bubbleContainer.appendChild(bubble);
+
+        // إزالة الفقاعة بعد انتهاء الحركة
+        setTimeout(() => {
+            bubble.remove();
+        }, 5000);
+    }
+
+    // إنشاء فقاعات جديدة كل نصف ثانية
+    setInterval(createBubble, 500);
+    }
+
+});
