fdb1e4707bfeddb68afbd565f03f2d5fdcc2d4d5
TamadurOme <tmoya.omer@gmail.com>
2025-05-17 21:33:23 +0300

    Create new module for hr expense portal

--- expense_portal/models/__init__.py
+++ expense_portal/models/__init__.py
@@ -2,3 +2,4 @@ from . import custom_expense_portal
 from . import current_balance
 from . import invoices_payment
 from . import account_payment_custom
+
---
+++ hr_expense_portal/__manifest__.py
@@ -0,0 +1,16 @@
+
+{
+    'name': 'HR Expense Portal',
+    'author': 'Net4X Innovation',
+    'version': '1.0',
+    'category': 'Human Resources',
+    'summary': 'Custom Expense Portal for Employees',
+    'depends': ['base', 'expense_portal'],
+    'data': [
+        'views/portal_dashboard.xml',
+        'views/portal_expense_templates.xml',
+
+    ],
+    'installable': True,
+    'application': False,
+}
